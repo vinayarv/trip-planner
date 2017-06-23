c44a0c5268273744ca525a25ac5936486220c84c
vinayarv <vinayarv@users.noreply.github.com>
2017-06-23 11:11:26 -0400

    added route to have dbContent

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const db = require('./models');
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
+const Promise = require('bluebird');
 
+//Exporting each model
+const Hotels = db.Hotels;
+const Restaurants = db.Restaurants;
+const Activities = db.Activities;
+const Place = db.Place;
 
 app.use(volleyball);
 
@@ -17,7 +23,15 @@ app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true });
 
 app.get('/', function(req, res, next) {
-  res.render('index');
+  /*
+    Promise: hotels, Restaurants, Activities
+  */
+  var hotelPromise = Hotels.findAll();
+  var restaurantsPromise = Restaurants.findAll();
+  var activitiesPromise = Activities.findAll();
+  Promise.all([hotelPromise, restaurantsPromise, activitiesPromise]).then(function(dbContent){
+      res.render('index', {templateHotels: dbContent[0], templateRestaurants: dbContent[1], templateActivities: dbContent[2]});
+  })
 });
 
 // add routes here
